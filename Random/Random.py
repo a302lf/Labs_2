8f4cb2017531071b01e9f46b20c4b1beb3ffb03d
a302lf <71495392+a302lf@users.noreply.github.com>
2021-05-16 04:54:26 +0300

    Create Random.py

---
+++ Random/Random.py
@@ -0,0 +1,11 @@
+from def_random import form_pas
+len = int(input("Input lenght of pass "))
+num = int(input("Input count of pass "))
+for i in range(num): 
+  print (f'Your pass: {form_pas(len)}')
+
+
+
+
+
+
